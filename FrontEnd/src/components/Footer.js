51b9f8c48b6d24c12d5a7023fd208611b07a6d72
Kuldeep Singh <kuldeepsingh2670@gmail.com>
2023-06-11 20:05:20 +0530

    FrontEnd Added

---
+++ FrontEnd/src/components/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+function Footer(){
+    return(
+        <footer className="footer">
+        <a href="https://github.com/29th-pixel/Plant-Disease-Detection-App" target="_blank" rel="noopener noreferrer">
+            <i className="fa-brands fa-github fa-xl" ></i>
+        </a>
+        </footer>
+    );
+}
+export default Footer;
